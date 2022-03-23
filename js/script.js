cbbbc941e40742a31845ec84fae5af70d99c1a79
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-03-23 13:02:00 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Anita Kay All rights reserved
+// Created by: Anita Kay
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calculateArea () {
+  // this function calculates area
+  document.getElementById('multiply-math').innerHTML = 'The area is ' + (13 * 17) + ' cm²'
+}
+
+function calculatePerimeter () {
+  // this function calculates perimeter
+  document.getElementById('add-math').innerHTML = 'The perimeter is ' + (13 * 2 + 17 * 2) + ' cm'
+}
